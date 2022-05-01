2d2893ae3d1b6d8259dc254110669815bd36257e
Hamidreza <hamidlashgari1998@gmail.com>
2022-05-01 19:43:03 +0430

    Add components to app

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react'
+import CardsList from '../components/card/CardList'
+import request from '../tools/request'
+
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    request('/Cards')
+      .then((res) => {
+        setItems(res.data)
+      })
+      .finally(setLoading(false))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{textAlign:"center"}}>Marketing Plugins</h1>
+        <h1 style={{ textAlign: 'center' }}>Marketing Plugins</h1>
       </header>
+      <CardsList items={items} isLoading={isLoading}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
