d2da0292aace9961af8aa55ad1fbdbf16d2a84c7
Hamidreza <hamidlashgari1998@gmail.com>
2022-05-02 20:52:50 +0430

    Add React router to app

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,32 +1,33 @@
 import PluginList from "../components/plugin/list/PluginList";
-import {useEffect, useState} from 'react'
-import CardsList from '../components/card/CardList'
-import request from '../tools/request'
+import { useEffect, useState } from "react";
+import CardsList from "../components/card/CardList";
+import request from "../tools/request";
+import { BrowserRouter as RouterProvider } from "react-router-dom";
 
 function App() {
-  const [items, setItems] = useState([])
-  const [isLoading, setLoading] = useState(true)
+  const [items, setItems] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    request('/Cards')
+    request("/Cards")
       .then((res) => {
-        setItems(res.data)
+        setItems(res.data);
       })
-      .finally(setLoading(false))
-  }, [])
+      .finally(setLoading(false));
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-6xl font-bold text-center">
-          Marketing Plugins
-        </h1>
-      </header>
-      <PluginList/>
-      <h1 style={{textAlign: 'center'}}>Marketing Plugins</h1>
-      <CardsList items={items} isLoading={isLoading}/>
+      <RouterProvider>
+        <header className="App-header">
+          <h1 className="text-6xl font-bold text-center">Marketing Plugins</h1>
+        </header>
+        <PluginList />
+        <h1 style={{ textAlign: "center" }}>Marketing Plugins</h1>
+        <CardsList items={items} isLoading={isLoading} />
+      </RouterProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
