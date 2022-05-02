666243866172c64cb46d0ee5f14d66a0073bed42
Hamidreza <hamidlashgari1998@gmail.com>
2022-05-02 21:25:52 +0430

    Create card component and path

--- src/routes/CustomRoute.js
+++ src/routes/CustomRoute.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Card from "../components/card/Card";
 import CardsList from "../components/card/CardList";
 import Error404 from "../components/Pages/Error404";
 
@@ -9,6 +10,7 @@ const CustomRoute = ({items, isLoading}) => {
     <div>
       <Routes>
         <Route path="/" element={<CardsList items={items} isLoading={isLoading} />} />
+        <Route path="/card/:id" element={<Card items={items} />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
