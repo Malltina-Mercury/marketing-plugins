9c45c7246f1a195c56c0635348bb1c7e84c4835c
Masud Afsar <masud@afsar.info>
2022-05-03 11:06:16 +0430

    Implement plugins list

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,29 +1,13 @@
-import {useEffect, useState} from "react";
-import request from "../tools/request";
 import Router from "../routes/Router";
 
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    request("/Cards")
-      .then((res) => {
-        setItems(res.data);
-      })
-      .finally(setLoading(false));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-6xl font-bold text-center">Marketing Plugins</h1>
+        <h1 className="text-3xl font-bold text-center">Marketing Plugins</h1>
       </header>
-      {/*<PluginList/>*/}
-      {/*<CustomRoute items={items} isLoading={isLoading}/>*/}
       <Router/>
-      <h1 style={{textAlign: "center"}}>Marketing Plugins</h1>
     </div>
   );
 }
